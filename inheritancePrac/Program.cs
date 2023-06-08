05f8504f5a3834da04710f23da97e185abe8f03a
Chaeyoung Moon <s2123@e-mirim.hs.kr>
2023-06-08 15:01:44 +0900

    클래스 변수 연습

--- inheritancePrac/Program.cs
+++ inheritancePrac/Program.cs
@@ -12,13 +12,21 @@ namespace inheritancePrac
       
         class Parent
         {
+            public static int counter = 0;
+
             public Parent() { Console.WriteLine("부모 생성자");  }
             public Parent(int param) { Console.WriteLine("Parent (int param)");  }
             public Parent(string param) { Console.WriteLine("Parant(string param)");  }
+
+            public void ContentParent()
+            {
+                Parent.counter++;
+            }
         }
 
         class Child : Parent 
         { 
+            public void CountChild() { Child.counter++; }
             public Child()  : base(10)
             {
                 Console.WriteLine("Child()  : base(10)");
